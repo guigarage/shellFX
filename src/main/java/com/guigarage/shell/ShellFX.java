26a1c12100c584c100c7fe91351e912a14889ae1
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-04-22 13:24:58 +0200

    readme

--- src/main/java/com/guigarage/shell/ShellFX.java
+++ src/main/java/com/guigarage/shell/ShellFX.java
@@ -2,9 +2,11 @@ package com.guigarage.shell;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import jdk.jshell.JShell;
 import jdk.jshell.SnippetEvent;
@@ -14,22 +16,23 @@ import java.util.List;
 
 
 public class ShellFX extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        TextField textField = new TextField();
+        JShell shell = JShell.builder().build();
 
+        TextField textField = new TextField();
+        Button evalButton = new Button("eval");
         ListView<String> listView = new ListView<>();
 
-        JShell shell = JShell.builder().build();
-        textField.setOnAction(e -> {
+        evalButton.setOnAction(e -> {
             List<SnippetEvent> events = shell.eval(textField.getText());
             events.stream().map(event -> convert(event)).filter(s -> s != null).forEach(s -> listView.getItems().add(s));
         });
 
         BorderPane pane = new BorderPane();
-        pane.setTop(textField);
+        pane.setTop(new HBox(textField, evalButton));
         pane.setCenter(listView);
         Scene scene = new Scene(pane);
         primaryStage.setScene(scene);
